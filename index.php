cfc3ebd6f92066f28252ee9a5aa11d23ab69a4fe
Dihep <dihep.demayo@gmail.com>
2024-06-10 18:30:39 +0200

    M1- Acquisizione dati.

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invia dati</title>
 </head>
+
 <body>
+
+    <form action="server.php" method="GET">
+        <input type="text" name="userInput" placeholder="Scrivi del testo">
+        <button type="submit">Invia</button>
+    </form>
     
 </body>
+
 </html>
