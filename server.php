6c54a38915962c7bbd4c05c4e06f97d7e8f5d9fb
Dihep <dihep.demayo@gmail.com>
2024-06-10 18:37:25 +0200

    M1- Ricezione e stampa dati.

--- server.php
+++ server.php
@@ -1,3 +1,7 @@
+<?php
+$input = $_GET["userInput"]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maneggia dati</title>
 </head>
+
 <body>
-    
+
+    <h1>Il testo inserito è:</h1>
+    <h2> <?php echo $input; ?> </h2>
+
 </body>
+
 </html>
